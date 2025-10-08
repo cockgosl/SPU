fe6ffe8f2c842eabb8ee7373d51e3301ee5f7428
cockgosl <Igra.nikolaev@gmail.com>
2025-10-08 17:13:52 +0300

    Add files via upload
    the first start of my SPU( it can calculate!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)

--- src/SPU.cpp
+++ src/SPU.cpp
@@ -4,26 +4,143 @@ int main() {
     SPU_t SPU1 = {};
     FILE* bytecode = fopen ("asm+SPU/asambler+bytecode/bytecode.txt", "rb");
     SPUInit (&SPU1, bytecode, sizeof(type));
+
+    SPUDo (&SPU1);
+
 }
 
 SPUErorr_t SPUInit( SPU_t* spu, FILE* bytecode, size_t value) {
     if (bytecode && spu) {
 
-        size_t capacity = Capacity(bytecode);
+        StackInit (&(spu->stack), 5, value);
+
+        ByteCodeInit(spu, bytecode, value);
         
-
-        printf ("capacity: %lu\n", capacity);
-        
-
-
-        //StackInit(&(spu->stack), )
     }
     return STACK;
 }
 
-size_t Capacity (FILE* bytecode) {
+SPUErorr_t SPUDestroy (SPU_t* spu) {
+    return STACK;
+}
+
+SPUErorr_t ByteCodeInit (SPU_t* spu, FILE* bytecode, size_t value) {
     char* buffer = ReadBuffer(bytecode);
     int capacity = atoi(strchr(buffer, '|') + 1);
+
+    spu->Bytecode = (type*) calloc (capacity, value);
+
+    size_t in = 0;
+
+    while (buffer[in] != '\0' || buffer[in] == '|') { 
+        if (buffer[in] == ',') { 
+            buffer[in] = '\0';
+        }
+        in++;
+    }
+
+    char* pointer = buffer;
+
+    for (size_t counter = 0; counter < capacity; counter++) {
+        (spu->Bytecode)[counter] = atoi (pointer);
+        pointer += strlen(pointer) + 1;
+    }
+
     free (buffer);
-    return capacity;
+    return STACK;
+}
+
+SPUErorr_t SPUDo (SPU_t* spu) {
+    type temp = 0;
+    char tempstr[100] = {};
+    double tempd = NAN;
+
+    type value = 0;
+    size_t capacity = 1;
+    size_t indicator = 0;
+
+    for (size_t in = 0; spu->Bytecode[in] != 0; in++) {
+        capacity++;
+    }
+
+    for (size_t in = 0; in < capacity; in++) {
+        switch ((spu->Bytecode)[spu->InstrPointer]) {
+            case 1:
+                spu->InstrPointer++;
+                StackPush (&(spu->stack), (spu->Bytecode)[spu->InstrPointer]);
+                spu->InstrPointer++;
+                break;
+            case 2:
+                spu->InstrPointer++;
+                StackPop (&(spu->stack), &value);
+                break;
+            case 3:
+                spu->InstrPointer++;
+                StackPop (&(spu->stack), &value);
+                temp += value;
+                value = 0;
+                StackPop (&(spu->stack), &value);
+                temp += value;
+                StackPush (&(spu->stack), temp);
+                value = 0;
+                temp = 0;
+                break;
+            case 4:
+                spu->InstrPointer++;
+                StackPop (&(spu->stack), &value);
+                temp = value;
+                StackPop (&(spu->stack), &value);
+                value = temp - value;
+                StackPush (&(spu->stack), value);
+                break;
+                temp = 0;
+            case 5:
+                spu->InstrPointer++;
+                StackPop (&(spu->stack), &value);
+                temp = value;
+                StackPop (&(spu->stack), &value);
+                value = value* temp;
+                StackPush (&(spu->stack), value);
+                break;
+                temp = 0;
+            case 6:
+                spu->InstrPointer++;
+                StackDump(&(spu->stack), 1);
+                StackPop (&(spu->stack), &value);
+                temp = value;
+                StackPop (&(spu->stack), &value);
+                if (value) {
+                    value = temp / value;
+                    StackPush (&(spu->stack), value);
+                }
+                else {
+                    printf ("division on zero\n");
+                }
+                temp = 0;
+                break;
+            case 7:
+                spu->InstrPointer++;
+                StackPop (&(spu->stack), &value);
+                tempd = sqrt(double (value));
+                sprintf(tempstr, "%.0f", tempd);
+                value = atoi (tempstr);
+                StackPush (&(spu->stack), value);
+                for (int i; i < 100; i++) {
+                    tempstr[i] = 0;
+                }
+                tempd = 0;
+                break;
+            case 42:
+                ;
+            case 0:
+                printf("the end of process, value: %d\n", (spu->stack).array[0]);
+                spu->InstrPointer = 0;
+                indicator = 1;
+                break;
+        }
+        if (indicator) {
+            break;
+        }  
+    }
+    return STACK;
 }
--- src/asm.cpp
+++ src/asm.cpp
@@ -27,54 +27,139 @@ int main (int argc, char* argv[]) {
 
                 if (strcmp (pointer, "PUSH") == 0) {
                     counter++;
-                    fprintf (output, "%d", 1);
+                    fprintf (output, "%d,", 1);
                     pointer += strlen ("PUSH");
                     while(!isalpha(*pointer)) {
-                        if (*pointer != '\0') {
+                        if (isdigit(*pointer)) {
                             fprintf (output, "%c", *pointer );
                         }
                         pointer++;
                     }
+                    fprintf (output, ",");
                     counter++;
-                } 
+                }
+                else if (strcmp (pointer, "PUSHR") == 0) {
+                    counter++;
+                    fprintf (output, "%d,", 42);
+                    pointer += strlen ("PUSHR");
+                    while(!isalpha(*pointer)) {
+                        pointer++;
+                    }
+
+                    if (strcmp (pointer, "RAX") == 0) {
+                        fprintf (output, "%d", 1);
+                        pointer += strlen ("RAX");
+                    }
+                    else if (strcmp (pointer, "RBX") == 0) {
+                        fprintf (output, "%d", 2);
+                        pointer += strlen ("RBX");
+                    }
+                    else if (strcmp (pointer, "RCX") == 0) {
+                        fprintf (output, "%d", 3);
+                        pointer += strlen ("RCX");
+                    }
+                    else if (strcmp (pointer, "RDX") == 0) {
+                        fprintf (output, "%d", 4);
+                        pointer += strlen ("RDX");
+                    }
+                    else if (strcmp (pointer, "REX") == 0) {
+                        fprintf (output, "%d", 5);
+                        pointer += strlen ("REX");
+                    }
+                    else if (strcmp (pointer, "RFX") == 0) {
+                        fprintf (output, "%d", 6);
+                        pointer += strlen ("RFX");
+                    }
+                    else if (strcmp (pointer, "RGX") == 0) {
+                        fprintf (output, "%d", 7);
+                        pointer += strlen ("RGX");
+                    }
+                    else if (strcmp (pointer, "RHX") == 0) {
+                        fprintf (output, "%d", 8);
+                        pointer += strlen ("RHX");
+                    }
+                    else if (strcmp (pointer, "RIX") == 0) {
+                        fprintf (output, "%d", 9);
+                        pointer += strlen ("RIX");
+                    }
+                    else if (strcmp (pointer, "RJX") == 0) {
+                        fprintf (output, "%d", 10);
+                        pointer += strlen ("RJX");
+                    }
+                    else if (strcmp (pointer, "RKX") == 0) {
+                        fprintf (output, "%d", 11);
+                        pointer += strlen ("RKX");
+                    }
+                    else if (strcmp (pointer, "RLX") == 0) {
+                        fprintf (output, "%d", 12);
+                        pointer += strlen ("RLX");
+                    }
+                    else if (strcmp (pointer, "RMX") == 0) {
+                        fprintf (output, "%d", 13);
+                        pointer += strlen ("RMX");
+                    }
+                    else if (strcmp (pointer, "RNX") == 0) {
+                        fprintf (output, "%d", 14);
+                        pointer += strlen ("RNX");
+                    }
+                    else if (strcmp (pointer, "ROX") == 0) {
+                        fprintf (output, "%d", 15);
+                        pointer += strlen ("ROX");
+                    }
+                    else if (strcmp (pointer, "RPX") == 0) {
+                        fprintf (output, "%d", 16);
+                        pointer += strlen ("RPX");
+                    }
+                    else {
+                        ;
+                    }
+                    fprintf (output, ",");
+                    counter++;
+                }  
+
                 else if (strcmp (pointer, "POP") == 0) {
                     counter++;
-                    fprintf (output, "%d", 2);
+                    fprintf (output, "%d,", 2);
                     pointer += strlen ("POP");
+                }
+                else if (strcmp (pointer, "POPR") == 0) {
+                    counter++;
+                    fprintf (output, "%d,", 33);
+                    pointer += strlen ("POPR");
                 }
                 else if (strcmp (pointer, "ADD") == 0) {
                     counter++;
-                    fprintf (output, "%d", 3);
+                    fprintf (output, "%d,", 3);
                     pointer += strlen ("ADD");
                 }
                 else if (strcmp (pointer, "SUB") == 0) {
                     counter++;
-                    fprintf (output, "%d", 4);
+                    fprintf (output, "%d,", 4);
                     pointer += strlen ("SUB");
                 }
                 else if (strcmp (pointer, "MUL") == 0) {
                     counter++;
-                    fprintf (output, "%d", 5);
+                    fprintf (output, "%d,", 5);
                     pointer += strlen ("MUL");
                 }
                 else if (strcmp (pointer, "DIV") == 0) {
                     counter++;
-                    fprintf (output, "%d", 6);
+                    fprintf (output, "%d,", 6);
                     pointer += strlen ("DIV");
                 }
                 else if (strcmp (pointer, "SQRT") == 0) {
                     counter++;
-                    fprintf (output, "%d", 7);
+                    fprintf (output, "%d,", 7);
                     pointer += strlen ("SQRT");
+                }
+                else if(*pointer == '\0') {
+                    pointer++;
                 }
                 else if (strcmp (pointer, "HLT") == 0) {
                     counter++;
                     fprintf (output, "%d", 0);
                     fprintf (output, "|%lu", counter);
                     break;
-                }
-                else if(*pointer == '\0') {
-                    pointer++;
                 }
                 else {
                     fprintf (stderr, "syntax error\n");
